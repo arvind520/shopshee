60e1a47a44f8b18059007da89c4bdd9d5d67652f
Arvind Chauhan <40683239+arvind520@users.noreply.github.com>
2022-10-17 15:34:06 +0530

    Redux and Router setup
    setup redux and router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import ProductDetail from './components/ProductDetail';
+import ProductListing from './components/ProductListing';
+import Error404 from './components/Error404';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (<>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path='/' element={<ProductListing />} />
+        <Route path='/product/:productid' element={<ProductDetail />} />
+        <Route path='/*' element={<Error404 />} />
+      </Routes>
+    </Router>
+  </>
   );
 }
 
---
+++ src/components/Error404.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Error404 = () => {
+  return (
+    <h1>Error 404! <br /> Page Not Found!</h1>
+  )
+}
+
+export default Error404
